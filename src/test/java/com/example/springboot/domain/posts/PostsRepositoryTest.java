5a017661f93f3c7901146523301181a539beca29
choong <ksky9230@naver.com>
2023-01-12 01:32:36 +0900

    스프링부트와 AWS로 혼자 구현하는 웹서비스3(Jpa Auditing 테스트)

--- src/test/java/com/example/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/example/springboot/domain/posts/PostsRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -36,5 +37,25 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_확인() throws Exception {
+        //given
+        LocalDateTime now = LocalDateTime.of(2023, 1, 12,0,0,0);
+        postRepository.save(Posts.builder()
+                .title("제목")
+                .content("내용")
+                .author("저자")
+                .build());
+        //when
+        List<Posts> postsList = postRepository.findAll();
+        //then
+        Posts posts = postsList.get(0);
+        System.out.println("createDate = " + posts.getCreatedDate() + ", modifiedDate = " + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+
     }
 }
