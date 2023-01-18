a9863c8c046f5d448b3e4d3d2115137b6678ca78
choong <ksky9230@naver.com>
2023-01-18 18:38:48 +0900

    무중단 배포 스크립트를 만들기위한 profile 테스트

---
+++ src/test/java/com/example/springboot/web/ProfileControllerUnitTest.java
@@ -0,0 +1,45 @@
+package com.example.springboot.web;
+
+import org.junit.Test;
+import org.springframework.mock.env.MockEnvironment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class ProfileControllerUnitTest {
+
+    @Test
+    public void real_profile_조회 () {
+
+        //given
+        String expectedProfile = "real";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("oauth");
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void if_actvice_profile_notEmpty_default_조회 () {
+
+        //given
+        String expectedProfile = "default";
+        MockEnvironment env = new MockEnvironment();
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+}
